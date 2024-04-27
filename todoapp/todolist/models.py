89b9b5f490bba6263011cfd083757629c028efb4
Artsiom <pankkkou@icloud.com>
2024-04-27 16:38:15 +0200

    Создал модели, подключил к базе данных, сдела миграцию

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDo(models.Model):
+    title = models.CharField('Note name', max_length=500)
+    is_complete = models.BooleanField('Complete', default=False)
+    
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
+    
+    def __str__(self):
+        return self.title
